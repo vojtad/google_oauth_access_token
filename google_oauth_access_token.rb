118a64a49b5a69117a310f2460ccc7397063a1c0
Vojta Drbohlav <vojta.d@gmail.com>
2015-08-28 19:32:07 +0200

    * updated script output

--- google_oauth_access_token.rb
+++ google_oauth_access_token.rb
@@ -1,37 +1,37 @@
-require 'google/api_client'
 require 'google/api_client/client_secrets'
 require 'sinatra'
 require 'logger'
-require 'json'
 
 enable :sessions
 
-def logger; settings.logger end
-
-def calendar; settings.calendar; end
+def logger
+  settings.logger
+end
 
 def user_credentials
   # Build a per-request oauth credential based on token stored in session
   # which allows us to use a shared API client.
   @authorization ||= (
-    auth = settings.authorization.dup
-    auth.redirect_uri = to('/oauth2callback')
-    auth.update_token!(session)
-    auth
+  auth = settings.authorization.dup
+  auth.redirect_uri = to('/oauth2callback')
+  auth.update_token!(session)
+  auth
   )
+end
+
+def client_secrets
+  settings.client_secrets
 end
 
 configure do
   logger = Logger.new(STDOUT)
   logger.level = Logger::DEBUG
 
-  client = Google::APIClient.new(application_name: 'SlidesLive', application_version: '1.0')
-  youtube = client.discovered_api('youtube', 'v3')
-
-  client_secrets = Google::APIClient::ClientSecrets.load
-  authorization = client_secrets.to_authorization
+  secrets = Google::APIClient::ClientSecrets.load
+  authorization = secrets.to_authorization
   authorization.scope = 'https://www.googleapis.com/auth/youtube.upload'
 
+  set :client_secrets, secrets
   set :authorization, authorization
   set :logger, logger
 end
@@ -53,5 +53,27 @@ get '/oauth2callback' do
 end
 
 get '/' do
-  [200, {'Content-Type' => 'text/plain'}, {access_token: user_credentials.access_token, refresh_token: user_credentials.refresh_token, expires_in: user_credentials.expires_in, issued_at: user_credentials.issued_at}.to_json]
+  data = MultiJson.dump({
+                            'installed': ({
+                                'client_id': user_credentials.client_id,
+                                'client_secret': user_credentials.client_secret,
+                                'redirect_uris': client_secrets.redirect_uris,
+                                'auth_uri': client_secrets.authorization_uri,
+                                'token_uri': client_secrets.token_credential_uri,
+                                'access_token': user_credentials.access_token,
+                                'refresh_token': user_credentials.refresh_token,
+                                'id_token': user_credentials.id_token,
+                                'expires_in': user_credentials.expires_in,
+                                'expires_at': user_credentials.expires_at.to_i,
+                                'issued_at': user_credentials.issued_at.to_i
+                            }).inject({}) do |accu, (k, v)|
+                              # Prunes empty values from JSON output.
+                              unless v == nil || (v.respond_to?(:empty?) && v.empty?)
+                                accu[k] = v
+                              end
+                              accu
+                            end
+                        })
+
+  [200, {'Content-Type' => 'text/plain'}, data]
 end

9cf51df67341e2a7089466b0d74fe7a0c78d3eb8
Vojta Drbohlav <vojta.d@gmail.com>
2018-11-01 14:45:56 +0100

    Updated sinatra app to work.

--- google_oauth_access_token.rb
+++ google_oauth_access_token.rb
@@ -8,19 +8,12 @@ def logger
   settings.logger
 end
 
-def user_credentials
-  # Build a per-request oauth credential based on token stored in session
-  # which allows us to use a shared API client.
-  @authorization ||= (
-  auth = settings.authorization.dup
-  auth.redirect_uri = to('/oauth2callback')
-  auth.update_token!(session)
-  auth
-  )
-end
-
 def client_secrets
   settings.client_secrets
+end
+
+def authorization
+  settings.authorization
 end
 
 configure do
@@ -29,43 +22,56 @@ configure do
 
   secrets = Google::APIClient::ClientSecrets.load
   authorization = secrets.to_authorization
-  authorization.scope = 'https://www.googleapis.com/auth/youtube.upload'
+  authorization.scope = 'https://www.googleapis.com/auth/youtube'
+  authorization.additional_parameters = {'access_type': 'offline'}
+
+  auth_token_initialized = false
 
   set :client_secrets, secrets
   set :authorization, authorization
   set :logger, logger
+  set :auth_token_initialized, auth_token_initialized
 end
 
 before do
-  unless user_credentials.access_token || request.path_info =~ /^\/oauth2/
+  if not settings.auth_token_initialized
+    auth_token_initialized = true
+
+    authorization.redirect_uri = to('/oauth2callback')
+    authorization.update_token!(session)
+  end
+
+  if not authorization.access_token and not request.path_info =~ /^\/oauth2/
     redirect to('/oauth2authorize')
   end
 end
 
 get '/oauth2authorize' do
   # Request authorization
-  redirect user_credentials.authorization_uri.to_s, 303
+  redirect authorization.authorization_uri.to_s, 303
 end
 
 get '/oauth2callback' do
-  user_credentials.code = params[:code] if params[:code]
-  user_credentials.fetch_access_token!
+  authorization.code = params[:code] if params[:code]
+  r = authorization.fetch_access_token!
+
+  redirect to('/'), 303
 end
 
 get '/' do
   data = MultiJson.dump({
                             'installed': ({
-                                'client_id': user_credentials.client_id,
-                                'client_secret': user_credentials.client_secret,
+                                'client_id': authorization.client_id,
+                                'client_secret': authorization.client_secret,
                                 'redirect_uris': client_secrets.redirect_uris,
                                 'auth_uri': client_secrets.authorization_uri,
                                 'token_uri': client_secrets.token_credential_uri,
-                                'access_token': user_credentials.access_token,
-                                'refresh_token': user_credentials.refresh_token,
-                                'id_token': user_credentials.id_token,
-                                'expires_in': user_credentials.expires_in,
-                                'expires_at': user_credentials.expires_at.to_i,
-                                'issued_at': user_credentials.issued_at.to_i
+                                'access_token': authorization.access_token,
+                                'refresh_token': authorization.refresh_token,
+                                'id_token': authorization.id_token,
+                                'expires_in': authorization.expires_in,
+                                'expires_at': authorization.expires_at.to_i,
+                                'issued_at': authorization.issued_at.to_i
                             }).inject({}) do |accu, (k, v)|
                               # Prunes empty values from JSON output.
                               unless v == nil || (v.respond_to?(:empty?) && v.empty?)
